51582486e66bedb426a2e48c4e633ac63b03742a
Johannes Sarpola <johannes.sarpola@gmail.com>
2017-05-07 20:29:56 +0300

    AppIO and tests

---
+++ src/main/java/fi/johannes/Core/AppIO.java
@@ -0,0 +1,43 @@
+package fi.johannes.Core;
+
+import fi.johannes.Utilities.Logging.Logging;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/**
+ * Created by Johannes on 7.5.2017.
+ */
+public class AppIO {
+
+    /**
+     * Stream all files in folder
+     * @param inputFolder
+     * @return
+     * @throws IOException
+     */
+    public static Stream<String> streamAllFiles(String inputFolder) throws IOException {
+        return Files.walk(Paths.get(inputFolder))
+                .filter(Files::isRegularFile)
+                .flatMap(f -> {
+                    try {
+                        return Files.lines(f);
+                    } catch (IOException e) {
+                        Logging.logMessage_Error(AppIO.class, "Could not read file "+f.getFileName());
+                        return null;
+                    }
+                });
+    }
+
+    /**
+     * Stream file
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    public static Stream<String> streamFile(String file) throws IOException {
+        return Files.lines(Paths.get(file));
+    }
+}
---
+++ src/test/groovy/fi/johannes/Core/AppIOTest.groovy
@@ -0,0 +1,43 @@
+package fi.johannes.Core
+
+import spock.lang.Specification;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by Johannes on 7.5.2017.
+ */
+class AppIOTest extends Specification {
+
+    def "test stream all files in folder"()  throws Exception {
+        given:
+        String p = App.getResource("io-testing").getFile().getAbsolutePath();
+
+        when:
+        Stream<String> stream = AppIO.streamAllFiles(p);
+        List<String> streamList = stream.collect(Collectors.toList())
+
+        then:
+        streamList != null
+        streamList.size() == 3
+        streamList.find { it == "this is first file" }
+        streamList.find { it == "this is second file" }
+        streamList.find { it == "this is third file" }
+    }
+
+    def "test stream file"() throws Exception {
+        given:
+        String p = App.getResource("io-testing/first.txt").getFile().getAbsolutePath();
+
+        when:
+        Stream<String> stream = AppIO.streamAllFiles(p);
+        String join = stream.collect(Collectors.joining(""))
+
+        then:
+        join != null
+        join == "this is first file"
+
+    }
+
+}
